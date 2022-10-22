cb77e7a5208441c8c023b93a1cddde38c6a34b0d
1925chicken <minori.sano@yandex.com>
2022-10-22 17:24:58 +0900

    fixed

--- ntt/src/main.rs
+++ ntt/src/main.rs
@@ -1,21 +1,6 @@
 static MOD:i64 = 998244353;
 static G:i64 = 3;//generator of F_998244353
-#[allow(dead_code)]
-fn solve(read: &mut snio::Reader<std::io::StdinLock<'_>>) {
-    let n = read.usize();
-    let m = read.usize();
-    let mut a:Vec<i64> = (0..n).map(|_| read.i64()).collect();
-    let mut b:Vec<i64> = (0..m).map(|_| read.i64()).collect();
-    let c = convolve(&mut a,&mut b);
-    for i in 0..n + m - 1 {
-        if i != n + m  - 2{
-            print!("{} ",c[i]);
-        }else {
-            print!("{}\n",c[i]);
-        }
-    }
-}
-fn recursive_ntt(a:&mut Vec<i64>,inversion:i64) -> Vec<i64>{
+recursive_ntt(a:&mut Vec<i64>,inversion:i64) -> Vec<i64>{
     if a.len() == 1 {
         return a.to_vec(); 
     }
@@ -83,78 +68,3 @@ fn modpow(_n: i64, _t: i64, modulo: i64) -> i64 {
     }
     ret
 }
-//use proconio::input;
-fn main() {
-    let t = std::io::stdin();
-    let mut read = snio::Reader::new(t.lock());
-    let n = 1;
-    for _ in 0..n {
-        solve(&mut read);
-    }
-}
- 
-#[allow(dead_code)]
-pub mod snio {
-    pub struct Reader<R: std::io::BufRead> {
-        reader: R,
-        buf: std::collections::VecDeque<String>,
-    }
- 
-    impl<R: std::io::BufRead> Reader<R> {
-        pub fn new(reader: R) -> Self {
-            Self {
-                reader,
-                buf: std::collections::VecDeque::new(),
-            }
-        }
-        fn load(&mut self) {
-            while self.buf.is_empty() {
-                let mut s = String::new();
-                let length = self.reader.read_line(&mut s).unwrap();
-                if length == 0 {
-                    break;
-                }
-                self.buf.extend(s.split_whitespace().map(|s| s.to_owned()));
-            }
-        }
-        pub fn string(&mut self) -> String {
-            self.load();
-            self.buf.pop_front().unwrap_or_else(|| panic!("input ended"))
-        }
-        pub fn char(&mut self) -> char {
-            let string = self.string();
-            let mut chars = string.chars();
-            let res = chars.next().unwrap();
-            assert!(chars.next().is_none(), "invalid input!");
-            res
-        }
-        pub fn chars(&mut self) -> Vec<char> {
-            self.read::<String>().chars().collect()
-        }
-        pub fn read<T: std::str::FromStr>(&mut self) -> T
-            where
-                <T as ::std::str::FromStr>::Err: ::std::fmt::Debug,
-        {
-            self.string().parse::<T>().expect("Failed to parse the input.")
-        }
-    }
-    macro_rules! definition_of_reader_of_numbers {
-            ($($ty:tt,)*) => {
-                impl <R:std::io::BufRead> Reader<R> {
-                    $(
-                    #[inline]
-                    pub fn $ty (&mut self) -> $ty {
-                        self.read::<$ty>()
-                    }
-                    )*
-                }
-            }
-        }
-    definition_of_reader_of_numbers! {
-        u8,u16,u32,u64,u128,usize,
-        i8,i16,i32,i64,i128,isize,
-        f32,f64,
-    }
-}
-
-const INF:i64 = 1i64 << 60;
